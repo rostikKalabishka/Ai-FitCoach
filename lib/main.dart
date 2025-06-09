7fad496b140bbbc9a94dc8d1ac88dbabbf761cd7
RostikKalabishka <rostik31052002@gmail.com>
2025-06-09 14:00:31 +0300

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:ai_fit_coach/config/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
