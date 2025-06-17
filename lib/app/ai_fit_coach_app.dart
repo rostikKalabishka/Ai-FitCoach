dac1e80663f48596d0ec48d9da167641148bb6a4
RostikKalabishka <rostik31052002@gmail.com>
2025-06-17 15:50:52 +0300

    resolving conflicts

--- lib/app/ai_fit_coach_app.dart
+++ lib/app/ai_fit_coach_app.dart
@@ -1,8 +1,7 @@
 import 'package:ai_fit_coach/app/app_initializer.dart';
 import 'package:ai_fit_coach/blocs/settings_cubit/settings_cubit.dart';
 
-// import 'package:ai_fit_coach/features/auth/auth.dart';
-import 'package:ai_fit_coach/features/settings/settings_screen/view.dart';
+import 'package:ai_fit_coach/router/router.dart';
 import 'package:ai_fit_coach/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,10 +17,17 @@ class _AiFitCoachAppState extends State<AiFitCoachApp> {
   final _router = AppRouter();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: themeDark,
-        debugShowCheckedModeBanner: false,
-        home: WelcomeScreen());
+    return AppInitializer(
+      child: BlocBuilder<SettingsCubit, SettingsState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            title: 'Flutter Demo',
+            theme: themeDark,
+            debugShowCheckedModeBanner: false,
+            routerConfig: _router.config(),
+          );
+        },
+      ),
+    );
   }
 }
