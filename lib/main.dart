601ab9a82526757f300b9a2809d623cb9eeb0f14
adalekorei <dalekorei.work@gmail.com>
2025-07-26 23:13:38 +0300

    upload data to workout, small fix

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,6 @@ Future<void> main() async {
 
   await NotificationsApi().initNotifications();
 
-  // await exerciseDescription();
-
-  // await exerciseDescription1();
-
-  // await exerciseDescription2();
-
-  // await exerciseDescription3();
-
-  // await exerciseDescription4();
-
   runApp(const AiFitCoachApp());
 }
 
