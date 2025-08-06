fe77234746279c883a7687d9749bef6603aced36
RostikKalabishka <rostik31052002@gmail.com>
2025-08-06 13:33:06 +0300

    add crashlytics in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -29,6 +30,14 @@ Future<void> main() async {
     await NotificationsApi().initNotifications();
   }
 
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const AiFitCoachApp());
 }
 
