be801278d3542c2919e6d1f5cd8dc174982e2ed8
RostikKalabishka <rostik31052002@gmail.com>
2025-08-11 15:41:38 +0300

    add changes

--- lib/repositories/ads_repository/ads_repository.dart
+++ lib/repositories/ads_repository/ads_repository.dart
@@ -7,13 +7,13 @@ class AdsRepository implements AbstractAdsRepository {
   @override
   String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-      // return '${dotenv.env['ANDROID_ADS_UNIT_ID']}';
+      // return 'ca-app-pub-3940256099942544/6300978111';
+      return '${dotenv.env['ANDROID_ADS_UNIT_ID']}';
     } else if (Platform.isIOS) {
       //
-      //return '${dotenv.env['IOS_ADS_UNIT_ID']}';
+      return '${dotenv.env['IOS_ADS_UNIT_ID']}';
 
-      return 'ca-app-pub-3940256099942544/2934735716';
+      // return 'ca-app-pub-3940256099942544/2934735716';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -22,9 +22,11 @@ class AdsRepository implements AbstractAdsRepository {
   @override
   String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712'; // Тестовий ID для Android
+      return '${dotenv.env['ANDROID_INTERSTITIAL_AD_UNIT_ID']}';
+      // return 'ca-app-pub-3940256099942544/1033173712'; // Тестовий ID для Android
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910'; // Тестовий ID для iOS
+      return '${dotenv.env['IOS_INTERSTITIAL_AD_UNIT_ID']}';
+      // return 'ca-app-pub-3940256099942544/4411468910'; // Тестовий ID для iOS
     } else {
       throw UnsupportedError('Unsupported platform');
     }
